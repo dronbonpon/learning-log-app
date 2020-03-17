fba4389e061f02f0f2e77a3cdfae1f0d570b26d2
Анна <useremail@example.com>
2020-03-17 17:55:24 +0300

    Ready for deployment to heroku.

---
+++ learning_log/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('', include('learning_logs.urls')),
+]
